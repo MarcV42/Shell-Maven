b57c791c1ef751304a25c33454d9df57597ec406
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-08-25 13:49:06 +0200

    new built System.out.printf("Hello and welcome!");

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
 
 
             // Press Ctrl+D to start debugging your code. We have set one breakpoint
