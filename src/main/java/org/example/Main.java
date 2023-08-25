a4b161b8fe382c137f8e6c3f78eab2a3cd97956e
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-08-25 12:58:52 +0200

    new built

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+}
+    }
+
